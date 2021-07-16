cfe6362d72f17c580ec378847932e8f5cfb6fdba
Julio Fallace <julioscoffee@gmail.com>
2021-07-16 14:55:38 -0300

    concluindo bloco 8

---
+++ bloco-08/dia-5/default-parameters.js
@@ -0,0 +1,8 @@
+// const greeting = (user) => {
+//   const userDisplay = typeof user === 'undefined' ? 'usuário' : user;
+//   console.log(`Welcome ${userDisplay}!`);
+// };
+// greeting(); // Welcome usuário!
+
+const greeting = (user = 'usuário') => console.log(`Welcome ${user}!`);
+greeting(); // // Welcome usuário!
--- bloco-08/dia-5/object-property-shorthand.js
+++ bloco-08/dia-5/object-property-shorthand.js
@@ -1,8 +1,18 @@
+// const newUser = (id, name, email) => {
+//   return {
+//     id: id,
+//     name: name,
+//     email: email,
+//   };
+// };
+// console.log(newUser(54, 'isabella', 'isabella@email.com')); // { id: 54, name: 'isabella', email: 'isabella@email.com' }
+
 const newUser = (id, name, email) => {
   return {
-    id: id,
-    name: name,
-    email: email,
+    id,
+    name,
+    email,
   };
 };
+
 console.log(newUser(54, 'isabella', 'isabella@email.com')); // { id: 54, name: 'isabella', email: 'isabella@email.com' }
